c86a4ebde9185664e36ffeb2317d3db716d1a5f7
tim <tnicolas@student.42.fr>
2018-02-06 13:47:54 +0100

    ok

--- asm/includes/corewar.h
+++ asm/includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 16:28:54 by tnicolas          #+#    #+#             */
-/*   Updated: 2018/02/06 10:58:49 by tnicolas         ###   ########.fr       */
+/*   Updated: 2018/02/06 13:47:41 by tnicolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@
 # include <op.h>
 # include <libft.h>
 
+# define SUCCESS 0
+# define ERROR 1
+
 typedef struct		s_op
 {
 	char			*name;
 	int				nb_arg;
-	int				type_arg[3];
+	int				type_arg[MAX_ARGS_NUMBER];
 	int				opcode;
 	int				nb_cycle;
 	char			*descrition;
-	int				jesaispas1;
-	int				jesaispas2;
+	int				octet_type_arg;
+	int				jesaispas2;//carry ???
 }					t_op;
 
 extern t_op			op_tab[17];
@@ -34,6 +37,7 @@ typedef struct		s_line
 {
 	struct s_line	*next;
 	char			*line;
+	int				num_line;
 	int				size;
 }					t_line;
 
@@ -58,6 +62,6 @@ typedef struct		s_lst
 }					t_lst;
 
 void		ft_lst_add_end(t_lst **begin, t_lst *new);
-void		ft_handle_line(t_a *a, char *ln, int num_ln);
+int			ft_handle_line(t_a *a, char *ln, int num_ln);
 
 #endif
--- asm/srcs/ft_handle_line.c
+++ asm/srcs/ft_handle_line.c
@@ -5,19 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/06 13:28:19 by tnicolas          #+#    #+#             */
+/*   Updated: 2018/02/06 13:47:47 by tnicolas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_handle_line.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 16:42:32 by tnicolas          #+#    #+#             */
-/*   Updated: 2018/02/06 12:11:44 by tnicolas         ###   ########.fr       */
+/*   Updated: 2018/02/06 13:27:56 by tnicolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 **   ____________________________________________________________
 **   | ft_handle_line.c                                         |
-**   |     ft_start_i(12 lines)                                 |
+**   |     ft_err_msg(4 lines)                                  |
+**   |     ft_start_i(13 lines)                                 |
 **   |     ft_get_name(12 lines)                                |
-**   |     ft_check_arg(7 lines)                                |
-**   |     ft_handle_line(16 lines)                             |
-**   |     main(10 lines)                                       |
+**   |     ft_get_type(41 lines)                                |
+**   |         MEUUUU too many lines                            |
+**   |     ft_get_size_op(16 lines)                             |
+**   |     ft_check_arg(19 lines)                               |
+**   |     ft_handle_line(23 lines)                             |
+**   |     main(14 lines)                                       |
+**   | MEUUUU too many functions                                |
 **   ------------------------------------------------------------
 **           __n__n__  /
 **    .------`-\00/-'/
@@ -29,10 +46,11 @@
 
 #include <corewar.h>
 
-static void	ft_err_msg(t_a *a, int num_ln, char *txt)
+static void	ft_err_msg(t_a *a, t_line *new_ln, char *txt)
 {
-	ft_errprintf("{red}ERROR:{eoc} {yellow}%s.s\n\t{bold}line: %d{eoc} ->"
-			"{bold} %s{eoc}\n", "a->nomdufichier", num_ln, txt);
+	ft_errprintf("{red}ERROR:{eoc} {yellow}%s.s\n{eoc}"
+			"\t{yellow}{bold}line: %d{eoc} ->{bold} %s{eoc}\n"
+			"\t%s\n", "A->NOMDUFICHIER", new_ln->num_line, txt, new_ln->line);
 	exit(EXIT_FAILURE);
 }
 
@@ -41,7 +59,7 @@ static int	ft_start_i(t_a *a, char *ln)
 	int		i;
 
 	i = -1;
-	while (ft_isalpha(ln[++i]))
+	while (ft_strchr(LABEL_CHARS, ln[++i]) && ln[i])
 		;
 	if (ln[i] == LABEL_CHAR)
 	{
@@ -69,7 +87,7 @@ static char	*ft_get_name(char *ln)
 	return (ret);
 }
 
-static int	ft_get_type(t_a *a, char *arg, int num_ln)
+static int	ft_get_type(t_a *a, char *arg, t_line *new_ln)
 {
 	int		i;
 
@@ -84,9 +102,9 @@ static int	ft_get_type(t_a *a, char *arg, int num_ln)
 			if (i >= 1 && i <= REG_NUMBER)
 				return (T_REG);
 			else
-				ft_err_msg(a, num_ln, "reg number does not exist");
+				ft_err_msg(a, new_ln, "reg number does not exist");
 		}
-		ft_err_msg(a, num_ln, "syntax error in reg");
+		ft_err_msg(a, new_ln, "syntax error in reg");
 	}
 	else
 	{
@@ -97,7 +115,7 @@ static int	ft_get_type(t_a *a, char *arg, int num_ln)
 				;
 			if (arg[i] == '\0')
 				return ((arg[0] == DIRECT_CHAR) ? T_DIR : T_IND);
-			ft_err_msg(a, num_ln, "syntax error in label");
+			ft_err_msg(a, new_ln, "syntax error in label");
 		}
 		else if (ft_isdigit(arg[i]))
 		{
@@ -106,15 +124,15 @@ static int	ft_get_type(t_a *a, char *arg, int num_ln)
 				;
 			if (arg[i] == '\0')
 				return ((arg[0] == DIRECT_CHAR) ? T_DIR : T_IND);
-			ft_err_msg(a, num_ln, "invalid parameter");
+			ft_err_msg(a, new_ln, "invalid parameter");
 		}
-		ft_err_msg(a, num_ln, "invalid parameter");
+		ft_err_msg(a, new_ln, "invalid parameter");
 	}
-	ft_err_msg(a, num_ln, "syntax error");
+	ft_err_msg(a, new_ln, "syntax error");
 	return (0);
 }
 
-static int	ft_get_size_op(t_a *a, char *ln, t_op *op, char **arg, int num_ln)
+static int	ft_get_size_op(t_a *a, char *ln, t_op *op, char **arg, t_line *new_ln)
 {
 	int		sz;
 	int		i;
@@ -125,16 +143,16 @@ static int	ft_get_size_op(t_a *a, char *ln, t_op *op, char **arg, int num_ln)
 	while (++i < op->nb_arg)
 	{
 //		ft_printf("\t%d -> %s (%d)\n", op->type_arg[i], arg[i], ft_get_type(a, arg[i], num_ln));
-		type = ft_get_type(a, arg[i], num_ln);
+		type = ft_get_type(a, arg[i], new_ln);
 		if (!(op->type_arg[i] & type))
-			ft_err_msg(a, num_ln, "invalid parameter type");
-		sz += ((type & T_REG) ? REG_SIZE : 0) + ((type & T_DIR) ? DIR_SIZE : 0)
-			+ (( type & T_IND) ? IND_SIZE : 0);
+			ft_err_msg(a, new_ln, "invalid parameter type");
+		sz += ((type & T_REG) ? 1 : 0) + ((type & T_DIR) ? DIR_SIZE : 0)
+			+ (( type & IND_SIZE) ? 2 : 0);
 	}
 	return (sz);
 }
 
-static void	ft_check_arg(t_a *a, char *name, t_line *new_ln, char *ln, int num_ln)
+static void	ft_check_arg(t_a *a, char *name, t_line *new_ln, char *ln)
 {
 	char	*tmp;
 	int		i;
@@ -149,60 +167,64 @@ static void	ft_check_arg(t_a *a, char *name, t_line *new_ln, char *ln, int num_l
 	while (ft_strcmp(op_tab[++i].name, name) != 0)
 		;
 	op = op_tab[i];
-	new_ln->size += ft_get_size_op(a, ln, &op, arg, num_ln);
+	new_ln->size = 1 + op.octet_type_arg +
+		ft_get_size_op(a, ln, &op, arg, new_ln);
 	i = -1;
 	while (arg[++i])
 		ft_fruit(1, arg[i]);
 	ft_fruit(2, tmp, arg);
 }
 
-void		ft_handle_line(t_a *a, char *ln, int num_ln)
+int			ft_handle_line(t_a *a, char *ln, int num_ln)
 {
 	char	*name;
 	int		i;
 	t_line	*new_ln;
 
-	ft_printf("{yellow}%s: %s({bold}\"%s\"{eoc}{yellow}){eoc}\n", __FILE__, __func__, ln);
+//	ft_printf("{yellow}%s: %s({bold}\"%s\"{eoc}{yellow}){eoc}\n", __FILE__, __func__, ln);
 
 	name = NULL;
 	if (!(new_ln = malloc(sizeof(t_line))))
 		exit(EXIT_FAILURE);
 	new_ln->line = ft_strdup(ln);
 	new_ln->size = 0;
+	new_ln->num_line = num_ln;
 	i = ft_start_i(a, ln);
-	if (ln[i - 1] != '\0')
+	if (i == 0 || ln[i] != '\0')
 	{
 		name = ft_get_name(ln + i);
 		i += ft_strlen(name);
-		ft_check_arg(a, name, new_ln, ln + i, num_ln);
+		ft_check_arg(a, name, new_ln, ln + i);
 	}
 	ft_lst_add_end((t_lst**)&a->line, (t_lst*)new_ln);
 	ft_fruit(1, name);
 
-	ft_printf("\tsize: %d\n", new_ln->size);
+//	ft_printf("\tsize: %d\n", new_ln->size);
+	return (SUCCESS);
 }
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-#include <string.h>
-#include <ctype.h>
-
-int			main(int ac, char **av)
-{
-	int		i;
-	t_a		a;
-
-	(void)ac;
-	(void)av;
-	(void)i;
-	a.line = NULL;
-	a.nb_label = 0;
-	ft_handle_line(&a, "add r1,   r2 ,  r15", 12);
-	ft_handle_line(&a, "label: zjmp  %12", 12);
-	ft_handle_line(&a, "ldi :seksek, %15648, r7", 12);
-	ft_handle_line(&a, "autre_label:", 12);
-	return (0);
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//#include <limits.h>
+//#include <string.h>
+//#include <ctype.h>
+//
+//int			main(int ac, char **av)
+//{
+//	int		i;
+//	t_a		a;
+//
+//	(void)ac;
+//	(void)av;
+//	(void)i;
+//	a.line = NULL;
+//	a.nb_label = 0;
+//	ft_handle_line(&a, "add r1,   r12 ,  r15", 1);
+//	ft_handle_line(&a, "label: zjmp  %12", 2);
+//	ft_handle_line(&a, "ldi :seksek, %15648, r7", 3);
+//	ft_handle_line(&a, "autre_label:", 4);
+//	ft_handle_line(&a, "label3:label5: label120: aff r16", 5);
+//	return (0);
+//}
