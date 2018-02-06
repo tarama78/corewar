f54bb4c03fb34ffad3e656ed13e3352b85f1df6c
tim <tnicolas@student.42.fr>
2018-02-06 17:34:24 +0100

    ok

--- asm/srcs/ft_handle_line.c
+++ asm/srcs/ft_handle_line.c
@@ -6,7 +6,7 @@
 /*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/06 13:28:19 by tnicolas          #+#    #+#             */
-/*   Updated: 2018/02/06 17:26:41 by tnicolas         ###   ########.fr       */
+/*   Updated: 2018/02/06 17:34:18 by tnicolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -208,27 +208,27 @@ int			ft_handle_line(t_a *a, char *ln, int num_ln)
 }
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-#include <string.h>
-#include <ctype.h>
-
-int			main(int ac, char **av)
-{
-	int		i;
-	t_a		a;
-
-	(void)ac;
-	(void)av;
-	(void)i;
-	a.line = NULL;
-	a.nb_label = 0;
-	ft_printf("retour %d\n", ft_handle_line(&a, "add r1,   r12 ,  r15", 1));
-	ft_printf("retour %d\n", ft_handle_line(&a, "label: zjmp  %12", 2));
-	ft_printf("retour %d\n", ft_handle_line(&a, "ldi :seksek, %-15648, r7", 3));
-	ft_printf("retour %d\n", ft_handle_line(&a, "autre_label:", 4));
-	ft_printf("retour %d\n", ft_handle_line(&a, "label3:label5: label120: aff r16", 5));
-	return (0);
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//#include <limits.h>
+//#include <string.h>
+//#include <ctype.h>
+//
+//int			main(int ac, char **av)
+//{
+//	int		i;
+//	t_a		a;
+//
+//	(void)ac;
+//	(void)av;
+//	(void)i;
+//	a.line = NULL;
+//	a.nb_label = 0;
+//	ft_printf("retour %d\n", ft_handle_line(&a, "add r1,   r12 ,  r15", 1));
+//	ft_printf("retour %d\n", ft_handle_line(&a, "label: zjmp  %12", 2));
+//	ft_printf("retour %d\n", ft_handle_line(&a, "ldi :seksek, %-15648, r7", 3));
+//	ft_printf("retour %d\n", ft_handle_line(&a, "autre_label:", 4));
+//	ft_printf("retour %d\n", ft_handle_line(&a, "label3:label5: label120: aff r16", 5));
+//	return (0);
+//}
