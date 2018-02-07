e03a19fc6b83fb706efca68d64f646dda362a5fe
bcozic <bcozic@student.42.fr>
2018-02-07 18:58:29 +0100

    file size

--- asm/srcs/ft_label.c
+++ asm/srcs/ft_label.c
@@ -6,7 +6,7 @@
 /*   By: bcozic <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 17:21:37 by bcozic            #+#    #+#             */
-/*   Updated: 2018/02/06 20:51:40 by bcozic           ###   ########.fr       */
+/*   Updated: 2018/02/07 18:57:56 by bcozic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,5 @@ void		ft_label(t_a *data)
 		addr += current->size;
 		current = current->next;
 	}
+	data->size = addr;
 }
