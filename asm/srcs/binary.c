29adc7a4fe1ad9a1896a6534152fda70006b6e8e
Yohan NACACHE <ynacache@e2r9p4.42.fr>
2018-02-07 16:55:23 +0100

    ok

--- asm/srcs/binary.c
+++ asm/srcs/binary.c
@@ -6,7 +6,7 @@
 /*   By: ynacache <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 17:40:53 by ynacache          #+#    #+#             */
-/*   Updated: 2018/02/07 16:50:20 by ynacache         ###   ########.fr       */
+/*   Updated: 2018/02/07 16:54:04 by ynacache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,41 +35,15 @@ static void		ft_handle_args(int file, char *arg, t_label *label, int *cmpt, int 
 			value = ft_atoi(arg);
 		cmpt += i;
 		octet = (char*)&value;
-		printf("i  %d\n", i);
 		while (--i >= 0)
-		{
-			printf("octect %d", octet[i]);
 			ft_putchar_fd(octet[i], file);
-		}
 	}
-
-//
-//	i = 0;
-//	if (arg[0] == DIRECT_CHAR && arg[1] == LABEL_CHAR)
-//	{
-//		printf("adress --> %d\n", label->addr);
-//		printf("arg in handle arg --> %s", arg + 2);
-//		ft_putchar_fd((char)(label->addr >> 8), file);
-//		ft_putchar_fd((char)(label->addr << 8), file);
-//	}
-//	else
-//	{
-//
-//		printf("arg in handle arg --> %s et aoit -->%d\n", arg, ft_atoi(arg + 1));
-//		if (arg[0] != 'r')
-//			ft_putchar_fd((char)ft_atoi(arg + 1) >> 8, file);
-//		ft_putchar_fd((char)ft_atoi(arg + 1), file);
-//		cmpt += 2;
-//	}
 }
 
 static int		ft_typepara(char *arg)
 {
 	if (arg[0] == 'r')
-	{
-		printf("reg code %d\n", REG_CODE);
 		return (REG_CODE);
-	}
 	else if (arg[0] == DIRECT_CHAR)
 		return (DIR_CODE);
 	else
@@ -122,6 +96,7 @@ int				ft_binary(int file, t_a *data)
 
 	tmp = data->line;
 	cmpt = 0;
+		puts("yo1");
 	ft_putname_magic(file, data);
 	while (tmp != NULL)
 	{
@@ -135,17 +110,15 @@ int				ft_binary(int file, t_a *data)
 		while (++i < 17 && op_tab[i].name != 0
 				&& ft_strcmp(words[j - 1], op_tab[i].name) != 0)
 			;
-		printf("///////////line --> %s\n", tmp->line);
 		ft_dprintf(file, "%c", (char)op_tab[i].opcode);
 		cmpt += 1;
 		if (op_tab[i].octet_type_arg == 1)
 			ft_encoding(file, args, &cmpt);
+		puts("yo2");
 		while (args[++k])
-		{
-			printf("arg --> %s\n", args[k]);
 			ft_handle_args(file, args[k], data->label, &cmpt, i);
-		}
 		tmp = tmp->next;
+		puts("yo3");
 	}
 	return (SUCCESS);
 }
