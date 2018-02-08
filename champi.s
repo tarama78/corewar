bef7481592a8ff7af95985740df570ef13ed5068
bcozic <bcozic@student.42.fr>
2018-02-08 06:28:45 +0100

    champion 1.1

--- champi.s
+++ champi.s
@@ -1,12 +1,12 @@
 .name		"champ"
 .comment	"worst ?"
 
-			ld		%200, r8
-			ld		%200, r10
+			ld		%255, r8
+			ld		%250, r10
 			ld		%0, r9
-			ld		%4, r2
-			ld		%42, r4
-			ld		%4, r2
+			ld		%8, r2
+			ld		%4, r12
+			ld		%8, r2
 			live	%0
 			zjmp	%:life
 			sti		r1, %:life, %1
@@ -15,13 +15,14 @@
 ff:			live	%0
 			fork	%:ford
 life:		live	%0
-write:		sti		r4, r8, r9
-		#	sti		r4, r10, r9
-			ldi		r9, %:ff, r4
+write:		ldi		r9, %:ff, r4
+			ldi		r12, %:ff, r5
+			sti		r4, r8, r9
+			sti		r5, r10, r12
 			add		r9, r2, r9
+			add		r12, r2, r12
 			ld		%0, r11
 			zjmp	%:life
 ford:		live	%0
-			fork	%79
+			fork	%200
 			ld		%0, r9
-
