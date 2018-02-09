432672bf342ba7fc95b4c220725f44c3a7bf1a45
Yohan NACACHE <ynacache@e2r9p4.42.fr>
2018-02-09 16:17:15 +0100

    ok

--- corewar/includes/corewar.h
+++ corewar/includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 16:28:54 by tnicolas          #+#    #+#             */
-/*   Updated: 2018/02/09 15:34:36 by tnicolas         ###   ########.fr       */
+/*   Updated: 2018/02/09 16:16:55 by ynacache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,49 @@
 # define SUCCESS 0
 # define ERROR -1
 
-typedef struct		s_op
+typedef struct			s_op
 {
-	char			*name;
-	int				nb_arg;
-	int				type_arg[MAX_ARGS_NUMBER];
-	int				opcode;
-	int				nb_cycle;
-	char			*descrition;
-	int				octet_type_arg;
-	int				size_change;
-}					t_op;
+	char				*name;
+	int					nb_arg;
+	int					type_arg[MAX_ARGS_NUMBER];
+	int					opcode;
+	int					nb_cycle;
+	char				*descrition;
+	int					octet_type_arg;
+	int					size_change;
+}						t_op;
 
-extern t_op			g_op_tab[17];
+extern t_op				g_op_tab[17];
 
-typedef struct		s_a
+typedef struct			s_process
 {
-}					t_a;
+	struct s_process	*next;
+	int					num_player;
+	int					reg[NB_REG];
+	int					pc;
+	int					cycle_wait;
+	bool				carry;
+	bool				live;
+}						t_process;
+
+typedef struct			s_player
+{
+	char				name[NAME_SIZE + 1];
+	char				comment[COMMENT_SIZE + 1];
+	int					player_number_print;
+	int					last_live_cycle;
+	int					nb_live_total;
+	int					nb_live_current;
+	size_t				nb_process;
+}						t_player;
+
+typedef struct			s_a
+{
+	t_process			*process;
+	t_player			player[MAX_PLAYER];
+	int					cycle_to_die;
+	uint64_t			cycle;
+	uint8_t				mem[MEM_SIZE];
+}						t_a;
 
 #endif
