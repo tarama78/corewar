0644f36d3e5f1a5f0feae1f51fd7791436a2972a
Yohan NACACHE <ynacache@e2r8p2.42.fr>
2018-02-15 16:22:25 +0100

    ok

--- corewar/srcs/commands.c
+++ corewar/srcs/commands.c
@@ -6,51 +6,11 @@
 /*   By: bcozic <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 17:02:28 by bcozic            #+#    #+#             */
-/*   Updated: 2018/02/15 15:25:42 by ynacache         ###   ########.fr       */
+/*   Updated: 2018/02/15 16:22:03 by ynacache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
-
-int		check_cycle(t_process *prc, t_a *a)
-{
-	int		i;
-	int		nb_player;
-
-	if (prc->cycle_wait == -1)
-	{
-		i = -1;
-		while (g_op_tab[++i].opcode != a->mem[prc->pc] && g_op_tab[i].opcode)
-			;
-		prc->cycle_wait = g_op_tab[i].nb_cycle - 1;
-		if (a->mem_info[prc->pc].process == 0)
-		{
-			a->mem_info[prc->pc].process = 1;
-			nb_player = 0;
-			while (++nb_player <= a->num_of_player)
-				if (a->player[nb_player - 1].player_number_print
-						== prc->num_player)
-					break ;
-			a->mem_info[prc->pc].player_process = nb_player;
-		}
-	}
-	if (prc->cycle_wait == 0)
-		return (1);
-	return (0);
-}
-
-void	ft_curseur(t_process *prc, int pc, int curs, t_a *a)
-{
-	char	nb_player;
-
-	a->mem_info[pc].process = 0;
-	a->mem_info[curs].process = 1;
-	nb_player = 0;
-	while (++nb_player <= a->num_of_player)
-		if (a->player[nb_player - 1].player_number_print == prc->num_player)
-			break ;
-	a->mem_info[curs].player_process = nb_player;
-}
 
 int		rec_memory(char type, int *curs, t_a *a, int addr)
 {
@@ -89,6 +49,23 @@ int		rec_memory(char type, int *curs, t_a *a, int addr)
 	return (val);
 }
 
+int		check_type_2(int tmp, int i, int j)
+{
+	if ((tmp & 0x0003) == 1 &&
+			((g_op_tab[i].type_arg[j] & 0x01) != 0x1))
+		return (0);
+	else if ((tmp & 0x0003) == 2 &&
+			((g_op_tab[i].type_arg[j] & 0x02) != 0x02))
+		return (0);
+	else if ((tmp & 0x0003) == 3 &&
+			((g_op_tab[i].type_arg[j] & 0x04) != 0x04))
+		return (0);
+	if ((tmp & 0x0003) == 0 &&
+			((g_op_tab[i].type_arg[j] & 0x07) != 0x00))
+		return (0);
+	return (1);
+}
+
 int		check_type(t_process *prc, t_a *a)
 {
 	int		i;
@@ -109,17 +86,7 @@ int		check_type(t_process *prc, t_a *a)
 	while (++j < 4)
 	{
 		tmp = arg_code >> 6;
-		if ((tmp & 0x0003) == 1 &&
-				((g_op_tab[i].type_arg[j] & 0x01) != 0x1))
-			return (0);
-		else if ((tmp & 0x0003) == 2 &&
-				((g_op_tab[i].type_arg[j] & 0x02) != 0x02))
-			return (0);
-		else if ((tmp & 0x0003) == 3 &&
-				((g_op_tab[i].type_arg[j] & 0x04) != 0x04))
-			return (0);
-		if ((tmp & 0x0003) == 0 &&
-				((g_op_tab[i].type_arg[j] & 0x07) != 0x00))
+		if (check_type_2(tmp, i, j) == 0)
 			return (0);
 		arg_code = arg_code << 2;
 	}
